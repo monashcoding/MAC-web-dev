a0a77f0eacbd820ab582df692bd02bca8bea4b36
aprilxyc <aprilchi4@gmail.com>
2020-05-28 18:16:12 +1000

    initial commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# MAC x Collab | HTML & CSS Basics
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/anna-giang/pen/ExVbdja](https://codepen.io/anna-giang/pen/ExVbdja).
+
+
---
+++ src/script.js
@@ -0,0 +1,12 @@
+function link_to_social(element) {
+  if (element.id === "facebook") {
+    window.location.href = "https://facebook.com";
+  }
+  else if (element.id === "instagram") {
+    window.location.href = "https://instagram.com";
+  }
+  else if (element.id === "twitter") {
+    window.location.href = "https://twitter.com";
+  }
+  
+}
